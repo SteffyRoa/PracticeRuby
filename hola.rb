5d0fc7cad34099e3483b6b55b27d830875ca47a4
Steffy <85137930+SteffyRoa@users.noreply.github.com>
2022-08-08 12:44:13 +0100

    first commit

---
+++ hola.rb
@@ -0,0 +1,15 @@
+/Las variables no indican
+El interprete deduce el tipo de variable/
+nombre = "Steffany"
+edad= 30
+tutor=" Beibi"
+
+/Esto es valido/
+variable = "Uriel"
+variable=20
+
+/nombre reservado lo marca diferente/
+/no debes de color la letra con mayuscula/
+/variable que va a cambiar minuscula al principio
+constante mayuscula a principio/
+
